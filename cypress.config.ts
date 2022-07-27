8f524df86cca6d8e21b9ceebd05c3c4b43b06810
Daniel Calle <danielcalleco@gmail.com>
2022-07-27 13:36:19 -0500

    CSS selector test passed (#7)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     video: false,
     screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
-      
-      // modify config values examples
-      // config.defaultCommandTimeout = 10000
-
-      // IMPORTANT return the updated config object
+      config.defaultCommandTimeout = 20000
+      config.responseTimeout = 20000
       return config;
     },
   },
