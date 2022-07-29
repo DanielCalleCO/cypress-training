aa464dc7ba643f98940705c5c50847fa798c1b5c
Daniel Calle <danielcalleco@gmail.com>
2022-07-29 11:35:36 -0500

    CSS selector modified

--- cypress/page/payment-step.page.ts
+++ cypress/page/payment-step.page.ts
@@ -8,7 +8,7 @@ class PaymentPage {
         this.payment = "[ title = \"Pay by bank wire\" ]";
         this.confirm = "I confirm my order";
         this.button = "button";
-        this.title = "#center_column > div > p > strong";
+        this.title = ".cheque-indent > ";
     }
 
     public confirmBankWirePayment(): void {
